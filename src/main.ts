325a5f88c16f3b08de16a6359b222aa1a2f9f6c0
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-14 23:44:56 +0200

    img

--- src/main.ts
+++ src/main.ts
@@ -12,14 +12,14 @@ export default class MyGame extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('sky', 'assets/sky.png');
+        this.load.image('sol', 'assets/sol1.png');
         this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.image('bords', 'assets/platform.png');
     }
 
     create() {
         // Décor
-        this.add.image(0, 0, 'sky').setOrigin(0,0);
+        this.add.image(0, 0, 'sol').setOrigin(0,0);
 
         // Le Joueur
         this.player = this.physics.add.sprite(480, 250, 'dude');
@@ -31,7 +31,7 @@ export default class MyGame extends Phaser.Scene {
         }
         // Mur exterieur
         this.bords = this.physics.add.staticGroup();
-        this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(1,1);
+        this.bords.create(0, 10, 'bords').setOrigin(0,0).setScale(1,1);
         this.bords.create(0, 525, 'bords').setOrigin(0,0).setScale(2,1);
         this.bords.create(16, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngle(90);
         this.bords.create(978-8, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngle(90);
