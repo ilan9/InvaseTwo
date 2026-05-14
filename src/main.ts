018ff417a8cf3ab101e2425a32a099a7aa3e5b9a
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-14 23:17:22 +0200

    an

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ export default class MyGame extends Phaser.Scene {
         this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(2,1);
         this.bords.create(0, 525, 'bords').setOrigin(0,0).setScale(2,1);
         this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngle(90);
-        this.bords.create(978-25, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngles(90);
+        this.bords.create(978-25, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngle(90);
         
     }
 
