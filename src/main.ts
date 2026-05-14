cec440d4775c567c36757a9a75491f88ed738af8
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-14 23:16:35 +0200

    angle

--- src/main.ts
+++ src/main.ts
@@ -31,8 +31,11 @@ export default class MyGame extends Phaser.Scene {
         }
         // Mur exterieur
         this.bords = this.physics.add.staticGroup();
-        this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(2,5);
-        this.bords.create(0, 525, 'bords').setOrigin(0,0).setScale(2,5);
+        this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(2,1);
+        this.bords.create(0, 525, 'bords').setOrigin(0,0).setScale(2,1);
+        this.bords.create(0, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngle(90);
+        this.bords.create(978-25, 0, 'bords').setOrigin(0,0).setScale(2,1).setAngles(90);
+        
     }
 
     update() {
