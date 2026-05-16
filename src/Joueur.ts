7f319905399be89433362c468f6905dcd7442ba6
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-16 23:48:21 +0200

    passss

--- src/Joueur.ts
+++ src/Joueur.ts
@@ -99,6 +99,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
                 }
             }
             if (Phaser.Input.Keyboard.JustDown(this.keyC)){
+                console.log("pas d'arme")
                 if (this.armes.length > 1){
                     this.arme_equiped = this.armes[1]
                     console.log(`arme equiper est ${this.arme_equiped.nom}`)
