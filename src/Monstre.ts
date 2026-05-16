26787081659bb6b0f89e65084fddd9472e5f7d71
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-16 21:34:08 +0200

    avant

--- src/Monstre.ts
+++ src/Monstre.ts
@@ -25,14 +25,9 @@ export default class Monstre extends Phaser.Physics.Arcade.Sprite {
         super(scene, x+offset_x, y+offset_y, skin);
         scene.add.existing(this);         // Pour l'afficher à l'écran
         scene.physics.add.existing(this); 
-        this.setScale(0.1)
+        this.setScale(0.5)
         this.setBounce(1)
         this.setMass(10)
-        // On limite leur vitesse pour ne pas qu'ils accélèrent à l'infini
-        this.setMaxVelocity(70);
-        
-        // On ajoute de la friction pour qu'ils ne glissent pas comme sur de la glace
-        this.setDrag(800);
         
 
         this.point_degat = 10*level
@@ -43,32 +38,17 @@ export default class Monstre extends Phaser.Physics.Arcade.Sprite {
         let distanceJ1 = Phaser.Math.Distance.Between(this.x, this.y, joueur1.x, joueur1.y);
         let distanceJ2 = Phaser.Math.Distance.Between(this.x, this.y, joueur2.x, joueur2.y)
 
-        // On vérifie le joueur 1
-        if (joueur1.vie > 0 && (distanceJ1 <= distanceJ2 || joueur2.vie <= 0)) {
-            if (distanceJ1 > 35) {
-                // Il est loin : il accélère vers toi
-                this.scene.physics.accelerateToObject(this, joueur1, 300);
-            } else {
-                // Il est à portée d'attaque : ARRÊT TOTAL IMÉDIAT
-                this.setAcceleration(0, 0);
-                this.setVelocity(0, 0); 
+        if (joueur1.vie >0 && (distanceJ1 <= distanceJ2 || joueur2.vie <= 0)){
+            if (distanceJ1 > 16){
+                this.scene.physics.moveToObject(this, joueur1, 60);
             }
-        } 
-        // On vérifie le joueur 2
-        else if (joueur2.vie > 0) {
-            if (distanceJ2 > 35) {
-                this.scene.physics.accelerateToObject(this, joueur2, 300);
-            } else {
-                this.setAcceleration(0, 0);
-                this.setVelocity(0, 0);
+        }else if(joueur2.vie >0){
+            if (distanceJ2 > 16){
+                this.scene.physics.moveToObject(this, joueur2, 60);
             }
-        } 
-        else {
-            // Plus personne à manger : on s'arrête
-            this.setAcceleration(0, 0);
-            this.setVelocity(0, 0);
+        }else{
+            this.scene.physics.moveTo(this,978/2,550/2,80)
         }
-    
         
     }
     attaquer(joueur: Player):void{
