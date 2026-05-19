2f96644303172afbfbea803c134354219b9e5c59
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-20 00:09:11 +0200

    2

--- src/arme/Baril.ts
+++ src/arme/Baril.ts
@@ -4,32 +4,19 @@ import type Player from '../Joueur';
 
 export default class Barrel extends Arme {
 
-    public x :number
-    public y :number
-
     constructor(scene: Phaser.Scene, groupeBalles: Phaser.Physics.Arcade.Group,portee:number,level:number,cadence:number) {
         super(scene, groupeBalles, "Barrel", portee, level, cadence);
 
-        this.x=0
-        this.y=0
     }
 
     // L'ÉCRASEMENT (Override) du tir classique
     public tirer(joueur:Player, dep_x: number, dep_y: number, move_x: number, move_y: number): void {
         if (this.stoque > 0){
                 this.stoque--
-            if (move_x == 1){
-                var x = dep_x + 20
-            }else{
-                var x = dep_x
-            }if (move_y == 1){
-                var y = dep_y + 20
-            }else{
-                var y = dep_y
-            }
-            this.x=x
-            this.y=y
-            const barrel = this.scene.add.image(x,y, 'barrel');
+            let x = dep_x + 30*move_x
+            let y = dep_y + 30*move_y
+
+            const barrel = this.scene.physics.add.image(x,y, 'barrel');
             this.scene.events.emit("create_barrel", barrel);
     }else{
             joueur.console.ajouterMessage(`Plus de balle dans ${this.nom}`);
