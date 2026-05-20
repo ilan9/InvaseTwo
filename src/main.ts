af14c7b286f99fdccb96210d84efa12b19263a2c
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-20 17:44:49 +0200

    ss

--- src/main.ts
+++ src/main.ts
@@ -42,7 +42,7 @@ export default class MyGame extends Phaser.Scene {
         this.anims.create({
             key: 'anim_boom', // Le nom secret de l'animation
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 73 }), // S'il y a 9 images (de 0 à 8)
-            frameRate: 15, // Vitesse de l'animation (15 images par seconde)
+            frameRate: 60, // Vitesse de l'animation (15 images par seconde)
             repeat: 0, // 0 = L'animation ne se joue qu'une seule fois
             hideOnComplete: true // MAGIQUE : Le sprite devient invisible tout seul à la fin !
         });
