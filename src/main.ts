996f63e5ea007cf19791c99d8e405af8029473e2
ilan <155641665+ilan9@users.noreply.github.com>
2026-05-26 16:35:25 +0200

    zo

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ export default class MyGame extends Phaser.Scene {
         this.load.spritesheet('dude', 'assets/img/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.spritesheet('dude2', 'assets/img/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.spritesheet('zombie2', 'assets/img/zombie1.png',  { frameWidth: 32*4, frameHeight: 32*8,margin:1 });
-        this.load.spritesheet('zombie', 'assets/img/zombie.png',  { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('zombie', 'assets/img/zombie.png',  { frameWidth: 64, frameHeight: 96 });
         this.load.image('bord_hori', 'assets/img/bord_horizontal.png');
         this.load.image('bord_vert', 'assets/img/bord_vertical.png');
         this.load.json('donnees_vagues', 'assets/json/vague.json');
