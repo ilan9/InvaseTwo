35abef74d6f685a4449ca6c3950b23eebfa4eb31
ilan <155641665+ilan9@users.noreply.github.com>
2026-06-02 01:47:23 +0200

    debu

--- src/main.ts
+++ src/main.ts
@@ -341,7 +341,7 @@ const config: Phaser.Types.Core.GameConfig = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false
+            debug: true
         }
     }
 };
