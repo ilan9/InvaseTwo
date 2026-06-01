39c2068ea771e40a5b1af738b6b5b6373466bbd7
ilan <155641665+ilan9@users.noreply.github.com>
2026-06-01 19:19:22 +0200

    sasa

--- src/main.ts
+++ src/main.ts
@@ -25,8 +25,8 @@ export default class MyGame extends Phaser.Scene {
 
     preload() {
         this.load.image('sol', 'assets/img/sol2.png');
-        this.load.spritesheet('dude', 'assets/img/characters/character_1_frame16x20.png', { frameWidth: 16, frameHeight: 20 });
-        this.load.spritesheet('dude2', 'assets/img/dude.png', { frameWidth: 32, frameHeight: 48 });
+        this.load.spritesheet('dude', 'assets/img/characters/character_1_frame16x20.png', { frameWidth: 16, frameHeight: 21});
+        this.load.spritesheet('dude2', 'assets/img/dude.png', { frameWidth: 32, frameHeight: 48 }); 
         this.load.spritesheet('zombie2', 'assets/img/zombie1.png',  { frameWidth: 32*4, frameHeight: 32*8,margin:1 });
         this.load.spritesheet('zombie', 'assets/img/zombie.png',  { frameWidth: 64, frameHeight: 96 });
         this.load.image('bord_hori', 'assets/img/bord_horizontal.png');
